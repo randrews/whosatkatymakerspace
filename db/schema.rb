563f545d64018ee8ac11f93dbc573ea890bb7761
Ross Andrews <ross.andrews@gmail.com>
2016-06-30 18:51:52 -0500

    Added notify field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160415030518) do
+ActiveRecord::Schema.define(version: 20160630233632) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160415030518) do
     t.string   "name"
     t.boolean  "admin",                  default: false
     t.boolean  "approved",               default: false
+    t.boolean  "notify"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
